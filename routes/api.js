7a8fcdff66ed9b933a134fae95ee4cd1f8d7b93f
txquang2202 <tranxuanquang81@gmail.com>
2023-11-15 13:23:27 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -28,6 +28,23 @@ const initApi = (app) => {
   router.post("/protected", authenticateJWT);
   router.get("/getprofile/:id", getUserProfile);
   router.put("/editprofile/:id", upload.single("img"), editUser);
+  router.get("/", (req, res) => {
+    const welcomeHTML = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Welcome</title>
+        </head>
+        <body>
+            <h1>Welcome to our ZenClass!</h1>
+            <p>The Back End is running</p>
+        </body>
+        </html>
+    `;
+    res.send(welcomeHTML);
+  });
 
   return app.use("/api/v1/", router);
 };
