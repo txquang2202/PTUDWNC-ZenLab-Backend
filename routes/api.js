592438a7fd24cf5d4cd0f0494338ea6845f1a1fb
txquang2202 <tranxuanquang81@gmail.com>
2023-11-15 13:29:50 +0700

    add

--- routes/api.js
+++ routes/api.js
@@ -11,16 +11,6 @@ import upload from "../middleware/multer.js";
 
 const router = express.Router();
 // @param {*} app: express app
-
-const checkLogin = (req, res, next) => {
-  const nonSecurePath = ["/", "/signup", "/signin"];
-  if (nonSecurePath.includes(req.path)) return next();
-
-  if (user) {
-    next();
-  } else {
-  }
-};
 const initApi = (app) => {
   router.post("/signUp", createUser);
   router.post("/login", handleLogin);
@@ -28,23 +18,6 @@ const initApi = (app) => {
   router.post("/protected", authenticateJWT);
   router.get("/getprofile/:id", getUserProfile);
   router.put("/editprofile/:id", upload.single("img"), editUser);
-  router.get("/", (req, res) => {
-    const welcomeHTML = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Welcome</title>
-        </head>
-        <body>
-            <h1>Welcome to our ZenClass!</h1>
-            <p>The Back End is running</p>
-        </body>
-        </html>
-    `;
-    res.send(welcomeHTML);
-  });
 
   return app.use("/api/v1/", router);
 };
